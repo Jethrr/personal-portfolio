a8854b5c63225bac09338fc6a9a51e3afa58da34
Jether Omictin <jethomictin@gmail.com>
2025-03-20 18:55:28 +0800

    fix

--- src/components/ui/CardComponent.tsx
+++ src/components/ui/CardComponent.tsx
@@ -6,7 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { FaGithub } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
 // import { Button } from "@/components/ui/button";
 // import { Link } from "react-router-dom";
 
